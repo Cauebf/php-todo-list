80627bf7dcafd442f6f1f0f04a9982ffb735c357
Cauê Favarim <cauebrolesef@gmail.com>
2025-09-28 23:08:01 -0300

    add create task functionality

---
+++ actions/create.php
@@ -0,0 +1,22 @@
+<?php
+include_once '../config/conn.php';
+
+$description = filter_input(INPUT_POST, 'description'); // Get the description from the form, filtered to prevent SQL injection (equivalent to $_POST['description'])
+
+// if description is empty, redirect to index
+if (!$description) {
+    header("Location: ../index.php"); // Redirect to index page
+    exit; // Exit the script to prevent further execution of the code
+}
+
+try {
+    $stmt = $conn->prepare("INSERT INTO tasks (description) VALUES (:description)"); // Prepare the SQL statement with a placeholder to prevent SQL injection
+    $stmt->bindParam(':description', $description); // Bind the $description to the placeholder
+    $stmt->execute(); // Execute the statement
+
+    header("Location: ../index.php"); // Redirect to index page
+    exit;
+} catch (PDOException $e) {
+    echo "Erro: " . $e->getMessage();
+    exit;
+}
